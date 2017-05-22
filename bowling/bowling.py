a2175b85776f0dc8ab74479ff50c067490a49a25
eleijonmarck <ericle@kth.se>
2017-05-23 00:41:29 +0200

    refactored to make it easier

--- bowling/bowling.py
+++ bowling/bowling.py
@@ -3,18 +3,48 @@
 # -*- coding: utf-8 -*- #
 
 
+def score_game(throws):
+    game = Game(throws)
+    return sum(frame.score for frame in _frames(game))
+
+
+def _frames(game):
+    index = 0
+    for frame_index in range(0, 10):
+        frame = _create_frame(game, index)
+        yield frame
+        index += frame.length
+
+
 def _create_frame(game, index):
     if _is_strike(game, index):
         return _create_strike(game, index)
     if _is_spare(game, index):
-        return _is_spare(game, index)
+        return _create_spare(game, index)
     return _create_normal_frame(game, index)
 
 
 def _create_strike(game, index):
-    look_ahead = 3
-    score = game.pins_knocked_down(index, look_ahead)
+    score = game.pins_knocked_down(index, 3)
     return Frame(score, 1)
+
+
+def _create_spare(game, index):
+    score = game.pins_knocked_down(index, 3)
+    return Frame(score, 2)
+
+
+def _create_normal_frame(game, index):
+    score = game.pins_knocked_down(index, 2)
+    return Frame(score, 2)
+
+
+def _is_strike(game, index):
+    return game.pins_knocked_down(index, 1) == 10
+
+
+def _is_spare(game, index):
+    return game.pins_knocked_down(index, 2) == 10
 
 
 class Game(object):
@@ -32,12 +62,6 @@ class Game(object):
 
 
 class Frame(object):
-    def __init__(self, number_of_frames):
-        self.frame_total = 0
-        self.number_of_frames = number_of_frames
-
-    def score_first(self, first_score):
-        self.first = first_score
-
-    def score_second(self, second_score):
-        self.second = second_score
+    def __init__(self, score, length):
+        self.score = score
+        self.length = length
--- tests/bowling_test.py
+++ tests/bowling_test.py
@@ -2,22 +2,22 @@
 
 import unittest
 
-from bowling.bowling import Game
+from bowling.bowling import score_game
 
 
 class TestBowlingMethods(unittest.TestCase):
 
-    def setUp(self):
-        self.game = Game()
-
     def test_empty_game_has_score_of_zero(self):
-        self.assert_game_score(0, self.game.score_game([]))
+        self.assert_game_score(0, [])
 
     def test_one_game_with_specific_set_throws(self):
         self.assert_game_score(17, [5, 4, 1, 6, 1])
 
     def test_one_spare_gives_additional_points_in_frame_before(self):
-        self.assert_game_score(13, [5, 5, 3])
+        self.assert_game_score(16, [5, 5, 3, 0])
+
+    def test_score_of_spare_is_ten_plus_next_throw(self):
+        self.assert_game_score(17, [4, 6, 2, 3])
 
     def test_two_strikes_in_a_row(self):
         self.assert_game_score(30, [10, 10])
@@ -27,7 +27,7 @@ class TestBowlingMethods(unittest.TestCase):
 
     def assert_game_score(self, expected_score, game_played):
         self.assertEqual(
-            expected_score, self.game.score_game(game_played))
+            expected_score, score_game(game_played))
 
 
 if __name__ == '__main__':
