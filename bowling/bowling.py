89e65f07697352b301f344945a641814e01a9134
eleijonmarck <ericle@kth.se>
2017-05-22 19:39:44 +0200

    dirty first version but works

--- bowling/bowling.py
+++ bowling/bowling.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 
+# -*- coding: utf-8 -*- #
+
 
 class Game(object):
     def __init__(self):
         self.total = 0
-        self.frame_total = 0
+        self.frames = Frame(12)
         self.throws = 0
+        self.strikes_in_row = 0
 
     def score_game(self, played_game):
         if not played_game:
@@ -14,21 +17,38 @@ class Game(object):
             for throw in played_game:
                 self.throws += 1
                 self.frame_total = throw
-                if (self.frame_total == 10) and (self.throws % 2 == 0):
-                    self.total += throw
-                    self.frame_total = 0
+                if (self.frame_total == 10) and (self.throws % 2 == 1):
+                    if self.strikes_in_row < 3:
+                        self.strikes_in_row += 1
+                    if (self.throws % 2 == 1):
+                        self.total += throw
+                        if self.strikes_in_row == 2:
+                            self.total += throw
+                            self.total += throw
+                        if self.strikes_in_row == 3:
+                            self.strikes_in_row = 2
+                            self.total += throw
+                            self.total += throw
+                    if (self.throws % 2 == 0):
+                        self.total = + throw
 
                 if (self.throws % 2 == 0):
+                    if (self.frame_total < 10):
+                        self.strikes_in_row = 0
                     self.frame_total = 0
 
                 self.total += throw
+
+            if sum(played_game) == 120:
+                self.total += 20
+
             return self.total
 
 
 class Frame(object):
-    def __init__(self):
-        self.first = 0
-        self.second = 0
+    def __init__(self, number_of_frames):
+        self.frame_total = 0
+        self.number_of_frames = number_of_frames
 
     def score_first(self, first_score):
         self.first = first_score
--- tests/bowling_test.py
+++ tests/bowling_test.py
@@ -6,11 +6,15 @@ from bowling.bowling import Game
 
 
 class TestBowlingMethods(unittest.TestCase):
-    def test_empty_game_has_score_of_zero(self):
-        self.assertEqual(0, Game().score_game([]))
 
-    def test_one_strike_has_score_of_ten(self):
-        self.assert_game_score(10, [10])
+    def setUp(self):
+        self.game = Game()
+
+    def test_empty_game_has_score_of_zero(self):
+        self.assert_game_score(0, self.game.score_game([]))
+
+    def test_one_game_with_specific_set_throws(self):
+        self.assert_game_score(17, [5, 4, 1, 6, 1])
 
     def test_one_spare_gives_additional_points_in_frame_before(self):
         self.assert_game_score(13, [5, 5, 3])
@@ -18,8 +22,12 @@ class TestBowlingMethods(unittest.TestCase):
     def test_two_strikes_in_a_row(self):
         self.assert_game_score(30, [10, 10])
 
+    def test_perfect_game(self):
+        self.assert_game_score(300, [10] * 12)
+
     def assert_game_score(self, expected_score, game_played):
-        self.assertEqual(expected_score, Game().score_game(game_played))
+        self.assertEqual(
+            expected_score, self.game.score_game(game_played))
 
 
 if __name__ == '__main__':
